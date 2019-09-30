834378b2ebf0ec32abc41ef7bd774b9b96a58902
Zzqqbs <2021059872@qq.com>
2019-09-30 12:35:14 +0800

    20190923-1235

---
+++ spaces.bat
@@ -0,0 +1,13 @@
+@REM @Author: Admin
+@REM @Date:   2019-09-30 07:00:44
+@REM @Last Modified by:   Admin
+@REM Modified time: 2019-09-30 07:03:09
+
+@echo off
+chcp 65001
+echo 生成用于储存数据的路径
+mkdir DB
+
+echo 目录结构如下
+tree
+pause
